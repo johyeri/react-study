250e459dc5c0bdba5840f7b7e5180620823e76a9
johyeri <johello62@gmail.com>
2022-09-21 00:02:54 +0900

    Commit before Chapter 18

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,20 +1,21 @@
+import { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
-import { connect } from 'react-redux';
-import { increase, decrease } from '../modules/counter'
-import { bindActionCreators } from 'redux';
+import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
-  return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+// useDispatch 를 사용할 때는 useCallback 과 함께 사용하는 습관을 들일 것
+const CounterContainer = () => {
+  const number = useSelector(state => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+  return(
+    <Counter
+      number={number}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+    />
   );
 };
 
-export default connect(
-  state => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounterContainer);
+export default CounterContainer;
